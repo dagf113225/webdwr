92508b6407385397e6c77f8e2c4768534da0f432
dagf113225 <512444862@qq.com>
2018-11-17 22:50:51 +0800

    提交dwr项目1

---
+++ jsdwrjavademo/src/main/java/com/lixin/user/action/UserTask.java
@@ -0,0 +1,13 @@
+package com.lixin.user.action;
+
+public class UserTask {
+	
+	public  String  getCode()
+	{
+		System.out.println("UserTask  is getCode  start...");
+		
+		return "1234";
+	}
+	
+
+}
