484bf95563dfedd8804caf496a97f90fed6a3d53
dagf113225 <512444862@qq.com>
2018-11-23 12:03:55 +0800

    antv_dwr数据

--- usercrud/src/main/java/com/lixin/user/action/UserAction.java
+++ usercrud/src/main/java/com/lixin/user/action/UserAction.java
@@ -2,6 +2,9 @@ package com.lixin.user.action;
 
 import com.lixin.user.database.DB;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 public class UserAction {
 
 	// 检查用户登录
@@ -9,83 +12,102 @@ public class UserAction {
 		System.out.println("UserAction  is  checkUserLogin  start... ");
 
 		System.out.println(name + "," + pwd);
-		
-		//产生db类对象
-		DB db  = new DB();
-		
-		
-		boolean  flag=db.checkLogin(name, pwd);
-		if(flag)
-		{
+
+		// 产生db类对象
+		DB db = new DB();
+
+		boolean flag = db.checkLogin(name, pwd);
+		if (flag) {
 			return "成功";
 		}
-		
+
 		return "用户登录失败";
 	}
-	
-	//检查用户名是否唯一
-	public String  checkUserName(String name)
-	{
+
+	// 检查用户名是否唯一
+	public String checkUserName(String name) {
 		System.out.println("UserAction  is  checkUserName  start... ");
-		System.out.println("------------>"+name);
-		DB  db = new DB();
-		
-		boolean flag=db.checkName(name);
-		if(flag)
-		{
+		System.out.println("------------>" + name);
+		DB db = new DB();
+
+		boolean flag = db.checkName(name);
+		if (flag) {
 			return "fail";
-		}
-		else
-		{
+		} else {
 			return "success";
 		}
-		
-		
+
 	}
-	
-	//生成动态标题菜单
-	public  String[]   getMenuDatas()
-	{
+
+	// 生成动态标题菜单
+	public String[] getMenuDatas() {
 		System.out.println("UserAction  is  getMenuDatas  start... ");
-		
-		
-		DB  db  = new DB();
-		
-		String[]  menuDatas=db.getDBMenu();
-		
-		for(String datas:menuDatas)
-		{
+
+		DB db = new DB();
+
+		String[] menuDatas = db.getDBMenu();
+
+		for (String datas : menuDatas) {
 			System.out.println(datas);
 		}
-	
-		return  menuDatas;
+
+		return menuDatas;
 	}
-	
-	//生成下拉选择框的数据
-	public  Object[][]  getClassNames()
-	{
+
+	// 生成下拉选择框的数据
+	public Object[][] getClassNames() {
 		System.out.println("UserAction  is  getClassNames  start... ");
-		
-		DB db  = new DB();
-		
-		Object[][]  datas=db.getClassNames();
-		
-		return  datas;
+
+		DB db = new DB();
+
+		Object[][] datas = db.getClassNames();
+
+		return datas;
 	}
-	
-	//根据班级的编号获取这个班级的学生的姓名
-	public  String[]   getClassBindStus(String id)
-	{
+
+	// 根据班级的编号获取这个班级的学生的姓名
+	public String[] getClassBindStus(String id) {
 		System.out.println("UserAction  is  getClassBindStus  start... ");
-	
-		
-		DB db  = new DB();
-		String[] names=db.getClassToStuName(id);
-	     
+
+		DB db = new DB();
+		String[] names = db.getClassToStuName(id);
+
 		return names;
-	
+
 	}
-	
-	
+
+	// [[],[]]
+	public String getAntvGroupSex() {
+		System.out.println("UserAction  is  getAntvGroupSex  start... ");
+
+		DB db = new DB();
+
+		Object[][] datas = db.getGroupSex();
+
+		// [[],[],[]]
+		/**
+		 * 女3 
+		 * 男7
+		 */
+		// [{},{},{}]
+		// json数据格式[] json数组,{}json对象{键:值}
+		// 数据容器倒换
+		JSONArray array = new JSONArray();
+
+		for (int i = 0; i < datas.length; i++) {
+
+			// 在这里创建JSONObject对象--->键值
+			JSONObject jsonObj = new JSONObject();
+			jsonObj.put("sex", datas[i][0]);
+			jsonObj.put("number", datas[i][1]);
+			
+			array.add(jsonObj);
+
+		}
+		System.out.println(array.toString());
+
+		return array.toString();
+
+	}
 
 }
--- usercrud/src/main/java/com/lixin/user/database/DB.java
+++ usercrud/src/main/java/com/lixin/user/database/DB.java
@@ -210,5 +210,55 @@ public class DB {
 	
 		
 	}
+	
+	public  Object[][]  getGroupSex()
+	{
+		String sql="SELECT ssex,COUNT(*) FROM t_stus  GROUP  BY ssex";
+		
+		
+		Object[][] datas = null;
+
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+
+			ResultSet rs = pstmt.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData();
+
+			int column = rsmd.getColumnCount();
+
+			int count = 0;
+
+			while (rs.next()) {
+				count++;
+			}
+
+			// 完成二维数组创建，实例化
+			datas = new Object[count][column];
+
+			// 结果集回到第一行
+			rs.beforeFirst();
+
+			int row = 0;
+
+			while (rs.next())// 控制的是行
+			{
+				for (int i = 1; i <=column; i++)// 控制的是列
+				{
+					datas[row][i - 1] = rs.getObject(i);
+				}
+				row++;
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return datas;
+		
+		
+		
+		
+	}
 
 }
